53251cb564a2ba5418f146e1c8f92d16d0eddb78
Choong Jin Yao <54147253+charliechoong@users.noreply.github.com>
2020-09-26 20:56:59 +0800

    Update revisitgeometry.py

--- codeitsuisse/routes/revisitgeometry.py
+++ codeitsuisse/routes/revisitgeometry.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 def evaluate():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    
+    # dummy comment
     shapeCoordinates = data.get("shapeCoordinates")
     lineCoordinates = data.get("lineCoordinates")
 
